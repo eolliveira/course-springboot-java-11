2d76c8f82f2c2829e5b001716dbf25ae111aebfa
Erick Oliveira <erick.oliveira9607@gmail.com>
2021-06-12 18:37:01 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -13,8 +13,6 @@ import javax.persistence.Table;
 
 import com.example.course.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 @Entity
 @Table(name = "tb_order")
