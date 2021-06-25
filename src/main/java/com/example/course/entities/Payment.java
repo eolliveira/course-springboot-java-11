114766f5313b0c3d57dca68038a5c2f38badee68
Erick Oliveira <erick.oliveira9607@gmail.com>
2021-06-24 21:21:56 -0300

    Exception handling - findById

--- src/main/java/com/example/course/entities/Payment.java
+++ src/main/java/com/example/course/entities/Payment.java
@@ -14,7 +14,8 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_payment")
 public class Payment implements Serializable {
-
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
